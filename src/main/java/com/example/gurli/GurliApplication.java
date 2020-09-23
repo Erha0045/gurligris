1a28c3b5e23655d21bcfb2b1adcfc8ee8bcfb9e2
erhan basaran <e.basaran.111@gmail.com>
2020-09-23 15:09:13 +0200

    first commit

---
+++ src/main/java/com/example/gurli/GurliApplication.java
@@ -0,0 +1,13 @@
+package com.example.gurli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GurliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GurliApplication.class, args);
+    }
+
+}
